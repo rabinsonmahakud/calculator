14ce9de6ce0a04944e51fc585c5d97df131d9f52
Sourav <sourabhnayak1403@gmail.com>
2024-02-26 22:55:56 +0530

    Arithmatc and calculator files are added for math operation

---
+++ Arithmatic.java
@@ -0,0 +1,7 @@
+public class Arithmatic 
+{
+	public int add(int x, int y){
+		return x+y;
+	}
+	
+}
---
+++ Calculator.java
@@ -0,0 +1,8 @@
+public class Calculator 
+{
+	public static void main(String[] args) 
+	{
+		Arithmatic ar =new Arithmatic()
+	    System.out.println(ar.add(2+3));
+	}
+}
