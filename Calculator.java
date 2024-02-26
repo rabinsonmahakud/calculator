5826d9cb1f1a88cf6c9e8e9891121c0fc6f49d20
Sourav <sourabhnayak1403@gmail.com>
2024-02-26 23:02:18 +0530

    Calculator.java logic modified

--- Calculator.java
+++ Calculator.java
@@ -2,7 +2,8 @@ public class Calculator
 {
 	public static void main(String[] args) 
 	{
-		Arithmatic ar =new Arithmatic()
-	    System.out.println(ar.add(2+3));
+		Arithmatic ar =new Arithmatic();
+		int result = ar.add(2, 3);
+	    System.out.println("Addition result is: "+result);
 	}
 }
